441d09e2a26b7f8924539842b3a49410f22989a6
Glenn Hodgkinson <glenn@atlaswebdesign.com>
2014-05-03 16:01:55 -0400

    Cleaned up duplicate function

--- functions.php
+++ functions.php
@@ -1,21 +1,9 @@
 <?php
-
+// Register my primary navigation menu
 function register_my_menu() {
   register_nav_menu('primarynav-menu',__( 'Primary Navigation Menu' ));
 }
 add_action( 'init', 'register_my_menu' );
-
-
-
-function register_my_menus() {
-  register_nav_menus(
-    array(
-      'primarynav-menu' => __( 'Primary Navigation Menu' )
-    )
-  );
-}
-add_action( 'init', 'register_my_menus' );
-
 
 // Replaces the excerpt "more" text by a link
 function new_excerpt_more($more) {
